6c8efffd052e00c00c71ac31c521fe18221e58b0
lmcasella <lautaromcasella@gmail.com>
2022-07-21 19:08:46 -0300

    Error

--- equivalencias-front-end/src/components/Registro.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Registro = () => {
-    return <></>;
-};
-
-export default Registro;
--- equivalencias-front-end/src/components/atoms/Formulario/Formulario.js
+++
@@ -1,9 +0,0 @@
-import { styled, Grid } from '@mui/material';
-
-const Formulario = styled(Grid)`
-    width: 100%;
-    max-width: 100%;
-    height: 100%;
-`;
-
-export { Formulario };
